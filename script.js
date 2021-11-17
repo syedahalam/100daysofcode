8856f67a8199b12957b85fb06697a3d7b32f7df3
syedahalam <syedahalam@yahoo.com>
2021-11-17 12:23:24 -0500

    big o

---
+++ script.js
@@ -0,0 +1,12 @@
+const nemo = ['nemo']
+const findNemo = (array) =>{
+    let t0 = performance.now();
+    for(let i=0; i<array.length;i++){
+        if(array[i]==='nemo'){
+            console.log('Found Nemo :)')
+        }
+    }
+    let t1 = performance.now()
+    console.log(` call to find nemo took ${t1-t0} milliseconds`)
+}
+findNemo(nemo)
