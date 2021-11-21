14aac22e5b5f08b79f4dd449e758c6dee15fb9c8
syedahalam <syedahalam@yahoo.com>
2021-11-21 18:02:29 -0500

    array code solved

--- script.js
+++ script.js
@@ -47,40 +47,105 @@
 //     }
 // }
 
-class MyArray {
-    constructor(){
-        this.length = 0;
-        this.data = {}
-    }
-    get(index){
-        return this.data[index]
-    }
-    push(item){
-        this.data[this.length] = item
-        this.length++;
-        return this.length
-    }
-    pop(){
-        const lastItem = this.data[this.length-1]
-        delete this.data[this.length-1];
-        this.length--;
-        return lastItem;
-    }
-    delete(index){
-        const item = this.data[index];
-        this.shiftItems(index);
-    }
-    shiftItems(index){
-        for(let i = index; i<this.length-1;i++){
-            this.data[i]=this.data[i+1]
-        }
-        delete this.data[this.length-1];
-        this.length--;
-    }
-}
-const newArray = new MyArray();
-newArray.push('hi')
-newArray.push('you')
-newArray.push('!')
-newArray.pop()
-console.log(newArray);
+// 
+
+// const revFunct = (str) => {
+//     return str.split('').reverse().join('');
+// }
+// console.log(revFunct('my name is humna'))
+
+// const mergedSortedArrays = (arr1, arr2) => {
+//     const merArr = [];
+//     let i=0;
+//     let j=0;
+   
+// while(arr1[0]||arr2[0]){
+
+//     if(arr1[i]<arr2[j]){
+//         merArr.push(arr1[i])
+//         console.log(merArr)
+//         i++
+// }else{
+//     merArr.push(arr2[j])
+// }
+// }
+
+//     }
+
+  
+
+
+// mergedSortedArrays([2,4,6], [5,1,3])
+
+// function solution(str, ending){
+//    str.endsWith(ending);
+// }
+// solution('abcd', 'cd')
+
+// const posChar = (str) => {
+// 	let alphabets = 'abcdefghijklmnopqrstuvwxyz'.split('');
+// 	//  let alphabetsPosition =
+// 	let lCase = str.toLowerCase();
+// 	 let code = lCase.split('').map(x=>alphabets.indexOf(x)+1)
+// 	//  let alphabetsPosition = str.split('').map(x=>alphabets.indexOf(x)+1)
+// 	//  return alphabetsPosition
+// 	return code;
+// }
+// console.log(posChar("The sunset sets at twelve o' clock."));
+// function repeatStr (n, s) {
+
+//   return s.repeat(n);
+// }
+// console.log(repeatStr(3, 'hi'))
+// const eve = (array) => {
+//     let len = array.length;
+//     // console.log(len)
+//     if(len%2===0) return even
+// }
+// eve([1,2,3])
+
+// const evenOrOdd = (num) => {
+//     if(num%2===0) return 'even'
+//     else return 'odd'
+// }
+// console.log(evenOrOdd(5))
+
+// const evenOrOddArray = (array) => {
+// let sum = array.reduce((acc, value)=>acc+value, 0)
+// return (sum%2===0 || sum===[] || sum===[0])?'even':'odd'
+    
+//  }
+
+// console.log(evenOrOddArray([0]))
+// array = [2,5,1,2,3,5,1,2,4]
+//should return 2
+
+// const repeat = (arr) =>{
+//     // const rep = [];
+
+//         for(let i=0;i<arr.length;i++){
+//             for(let j=1;j<arr.length;j++){
+//                 if(arr[j]===arr[j+1]){
+//                    return arr[j] 
+//                 } else if(arr[i]===arr[j]){
+//                     return arr[i]
+//                 }return 'no recurrance'
+//             }
+//         }
+// }
+// console.log(repeat([1,2,1,4]));
+
+const repeat = (arr) => {
+	for (let i = 0; i < arr.length; i++) {
+		for (let j = 1; j < arr.length; j++) {
+			if(arr[i] === arr[j]) {
+				return arr[i];
+			} else if (arr[j]===arr[j+1]){
+                return arr[j]
+            }
+		
+		}
+        return 'no recurrance';
+	}
+};
+console.log(repeat([2, 1,1,2,3,5,1,2,4]));
