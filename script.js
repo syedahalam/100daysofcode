81992ef2aabb3b0611eb67e2b8f0aa4e732c60b8
syedahalam <syedahalam@yahoo.com>
2021-11-20 19:01:54 -0500

    arrays

--- script.js
+++ script.js
@@ -37,12 +37,50 @@
 // }
 // findNemo3(['nemo', 'dog'])
 
-class player {
-    constructor(name, type){
-        this.name = name;
-        this.type = type;
+// class player {
+//     constructor(name, type){
+//         this.name = name;
+//         this.type = type;
+//     }
+//     introduction(){
+//         console.log(`Hi, my name is ${this.name} and I am ${this.type}`)
+//     }
+// }
+
+class MyArray {
+    constructor(){
+        this.length = 0;
+        this.data = {}
     }
-    introduction(){
-        console.log(`Hi, my name is ${this.name} and I am ${this.type}`)
+    get(index){
+        return this.data[index]
+    }
+    push(item){
+        this.data[this.length] = item
+        this.length++;
+        return this.length
+    }
+    pop(){
+        const lastItem = this.data[this.length-1]
+        delete this.data[this.length-1];
+        this.length--;
+        return lastItem;
+    }
+    delete(index){
+        const item = this.data[index];
+        this.shiftItems(index);
+    }
+    shiftItems(index){
+        for(let i = index; i<this.length-1;i++){
+            this.data[i]=this.data[i+1]
+        }
+        delete this.data[this.length-1];
+        this.length--;
     }
 }
+const newArray = new MyArray();
+newArray.push('hi')
+newArray.push('you')
+newArray.push('!')
+newArray.pop()
+console.log(newArray);
