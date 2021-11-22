4789320b1daa5d5ae2e1ca1d4d0e09b4b47e213c
syedahalam <syedahalam@yahoo.com>
2021-11-21 20:24:44 -0500

    ll1

--- script.js
+++ script.js
@@ -174,5 +174,18 @@ class LinkedList{
         this.tail = this.head;
         this.length = 1;
     }
+    append(value){
+        const newNode = {
+            value: value,
+            next: null
+        }
+        this.tail.next = newNode;
+        this.tail = newNode;
+        this.length++
+    }
 }
 const myLinkedList = new LinkedList(10)
+myLinkedList.append(5)
+myLinkedList.append(15)
+
+console.log(myLinkedList)
