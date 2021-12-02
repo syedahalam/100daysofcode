f43b9365dd43977162171e74be7e30f6a4b5cd71
syedahalam <syedahalam@yahoo.com>
2021-12-01 22:09:12 -0500

    classes

--- script.js
+++ script.js
@@ -619,3 +619,27 @@
 //     console.log('syeda')
 // }
 // printMyName()
+ 
+class Human{
+    constructor(){
+        this.gender = 'female'
+    }
+    printGender(){
+        console.log(this.gender);
+    }
+}
+
+
+class Person extends Human{
+     constructor(){
+         super()
+         this.name ="Humna"
+         this.gender = "male"
+     }
+     printMyName(){
+         console.log(this.name);
+     }
+ }
+ const person = new Person()
+ person.printMyName()
+ person.printGender()
